348fc8bd72b354dc8be02db98701c06993a7969f
Ran Mishael <ran@lavanet.xyz>
2023-07-11 18:54:20 +0200

    adding automation to support automatically building the sdk seed from the public rpc endpoint of lava network

---
+++ utils/create_sdk_seed_json.py
@@ -0,0 +1,29 @@
+import subprocess
+import yaml
+import json
+result = subprocess.check_output(f"lavad q pairing providers LAV1 --node \"http://public-rpc.lavanet.xyz:80/rpc/\" --chain-id lava-testnet-1",shell=True)
+jsonFinal = {"testnet": {"1": [], "2": []}}
+yml = yaml.safe_load(result)
+for k in yml["stakeEntry"]:
+    address = k['address']
+    stake = int(k['stake']['amount'])
+    for i in k['endpoints']:
+        if i['useType'] == "tendermintrpc":
+            port = i['iPPORT']
+            geolocation = i['geolocation']
+            jsonFinal['testnet'][geolocation].append({
+            "rpcAddress": port,
+            "publicAddress": address,
+            "stake": stake
+            })
+
+def sortLambda(val):
+    return val["stake"]
+
+for i in ["1","2"]:
+    jsonFinal["testnet"][i].sort(key=sortLambda,reverse=True)
+    for j in jsonFinal["testnet"][i]:
+        del j["stake"]
+
+with open('sdkSeedProviders.json', 'w+') as f:
+    json.dump(jsonFinal, f, indent=4)
